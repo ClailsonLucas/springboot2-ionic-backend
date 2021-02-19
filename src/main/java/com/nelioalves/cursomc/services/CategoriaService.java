1e2b51b53a211437761f568b963035d4f3af3e41
Clailson Lucas <clailsonlucas@gmail.com>
2021-02-19 14:14:11 -0300

    Ajustes no endpoint /categorias/{id}. Obs: Error na classe CategoriaService, as requisições no Postman não funcionam.

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -1,9 +1,12 @@
 package com.nelioalves.cursomc.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -11,9 +14,10 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaService repo;
 	
-	public Categoria buscar(Integer id) {
-		Categoria obj = repo.buscar(id);
-		return obj;
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + "Tipo: " + Categoria.class.getName()));
 	}
 	
 }
